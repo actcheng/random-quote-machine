e4adeae24e369ac112f652c274e7ddb0dce8984f
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-05 23:12:59 +0900

    Rewritten using jQuery, and added fade in-out animations

--- randomQuoteMachine/app.js
+++ randomQuoteMachine/app.js
@@ -1,21 +1,40 @@
-let text = 'XD';
-let author = 'ME';
+$(document).ready(function() {
+
+});
+
+let body = document.body
 let text_div=document.getElementById("text");
 let author_div=document.getElementById("author");
 let newQuote_div=document.getElementById("new-quote");
 
+let colorList = ["#f993b7", "#8792e5","#a9e587","#f2aeae","#93a4a6",
+"#9b81b5","#afeca9","#dbb5f1","#e3f49c"]
+
+function randnElement(list){
+  let randn = Math.floor(Math.random()*list.length);
+  return list[randn];
+}
+
 function updateQuote() {
-  let randn = Math.floor(Math.random()*quotes.length);
-  quote = quotes[randn];
-  text_div.innerHTML = '"'+quote.text+'"';
-  author_div.innerHTML = '- '+quote.author;
+  let fadeTime = 500;
+  let {text,author} = randnElement(quotes);
+  $(text_div).fadeOut(fadeTime,function(){
+      $(this).text('"'+text+'"').fadeIn(fadeTime);
+    });
+  $(author_div).fadeOut(fadeTime,function(){
+      $(this).text('- '+author).fadeIn(fadeTime);
+    });
+  $(body).animate({backgroundColor: randnElement(colorList)}, fadeTime*2);
+
 }
 
 function main(){
-  updateQuote();
-  newQuote_div.addEventListener('click', function() {
+  $("#new-quote").click(function() {
     updateQuote();
-  })
+  });
 }
 
 main();
+
+
+//$("#quote-box").css("backgroundColor", "blue");
