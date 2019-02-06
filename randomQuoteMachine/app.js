b7dba05252dcb329af5d4d10f9884ea45fcf4d90
Actcheng <annacheng310@gmail.com>
2019-02-06 16:26:22 +0900

    Animate new-quote and tweet-quote

--- randomQuoteMachine/app.js
+++ randomQuoteMachine/app.js
@@ -6,6 +6,7 @@ let body = document.body
 let text_div=document.getElementById("text");
 let author_div=document.getElementById("author");
 let newQuote_div=document.getElementById("new-quote");
+let tweetQuote_div=document.getElementById("tweet-quote");
 
 let colorList = ["#f993b7", "#8792e5","#a9e587","#f2aeae","#93a4a6",
 "#9b81b5","#afeca9","#dbb5f1","#e3f49c"]
@@ -24,8 +25,10 @@ function updateQuote() {
   $(author_div).fadeOut(fadeTime,function(){
       $(this).text('- '+author).fadeIn(fadeTime);
     });
-  $(body).animate({backgroundColor: randnElement(colorList)}, fadeTime*2);
-
+  let newColor = randnElement(colorList)
+  $(body).animate({backgroundColor: newColor}, fadeTime*2);
+  $(newQuote_div).animate({backgroundColor: newColor}, fadeTime*2);
+  $(tweetQuote_div).animate({color: newColor}, fadeTime*2);
 }
 
 function main(){
